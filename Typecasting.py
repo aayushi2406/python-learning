82502c58c530ea4e898facda3fd03df62393c2fb
Aayushi Bhardwaj <aayushibhardwaj2406@gmail.com>
2026-05-16 20:22:48 +0530

    Type casting in python

---
+++ Typecasting.py
@@ -0,0 +1,12 @@
+age = input("enter your age : ")   #to take input from user
+print(age)  #print is used to display the output
+
+num1 = int(input("enter value 1 : "))  #input convert into int
+num2 = int(input("enter value 2 : "))
+print(num1 + num2)
+
+a = float(input("enter value : "))    #input convert into float
+print(a)
+
+b = eval(input("enter value : "))     #eval can take a string that represents a valid Python expression
+print(b)
