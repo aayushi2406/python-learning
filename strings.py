0e329de75b69743d43fa37fcd1fa8cfb5bc4abf8
Aayushi Bhardwaj <aayushibhardwaj2406@gmail.com>
2026-05-16 18:49:04 +0530

    strings in python

---
+++ strings.py
@@ -0,0 +1,10 @@
+a = "hello"
+b = "aayushi"
+print(a + b) #here "+" concatenate these two strings without space
+print(a + " " + b) #output : hello aayushi 
+
+c = 10
+d = 5
+print(c + d)  #here "+" add these two numbers . output = 15
+
+#print(a + c) -->ERROR (as a is string and c is integer)
