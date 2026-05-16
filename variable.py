1edda5f9ea7a87f0f354ea48d9afbfacb2c48d20
Aayushi Bhardwaj <aayushibhardwaj2406@gmail.com>
2026-05-16 18:34:52 +0530

    variables in python

---
+++ variable.py
@@ -0,0 +1,13 @@
+a = 10 
+b = 10
+print(id(a),id(b))   #same id (ek hi bar 10 ka allocation hoga)
+print(a)
+
+#RULES FOR VARIABLE NAME:
+"""
+1)cannot start with number.
+2)do not give space between variabes name.
+3)use underscore instead of space.
+4)except underscore(_) you can not use any special character in starting.
+5)python is case sensitive. A and a is treated different.
+"""
