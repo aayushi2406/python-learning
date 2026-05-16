cd0c3a17a9098c01e322ec78cb65b1fe8ce1eb5b
Aayushi Bhardwaj <aayushibhardwaj2406@gmail.com>
2026-05-16 20:06:29 +0530

    Data Types in python

---
+++ dataTypes.py
@@ -0,0 +1,40 @@
+#DATA TYPES: there are two types of data types
+#1)MUTABLE -> can change there value [ex = list,dictionary]
+#2)IMMUTABLE -> cannot change value [ex = string,tuple,set]
+
+#type() used to find the data type of a variable or value
+
+a = 5  #integer
+print(a,type(a))  
+
+b = 3.5 #float
+print(b,type(b))
+
+c = 1+2j #complex
+print(c,type(c))
+
+d = "aayushi" #string
+print(d,type(d))
+
+e = "32" #string
+print(e,type(e))
+
+list = [72,83,12,32]
+print(list,type(list))
+
+dict = {1 : "aayushi", 2 : "shivesh"}
+print(dict,type(dict))
+
+tuple = (2,4,3)
+print(tuple,type(tuple))
+
+set = {1,2,3,4}
+print(set,type(set))
+
+
+
+
+
+
+
+
