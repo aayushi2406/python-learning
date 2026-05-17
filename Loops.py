111869ac80922a4bce080c28a59e5a33bb575a16
Aayushi Bhardwaj <aayushibhardwaj2406@gmail.com>
2026-05-17 12:46:50 +0530

    Loops in python

---
+++ Loops.py
@@ -0,0 +1,54 @@
+#RANGE FUNCTION:
+"""
+range(5):
+start = 0
+condition < 5
+increment by 1 (by default)
+
+range(1,6):
+start = 1
+condition < 6
+increment by 1
+
+range(1,10,2):
+start = 1
+condition < 10
+increment by 2
+
+range(10,0,-1)
+start = 10
+condition > 0
+decrement by -1
+"""
+
+
+#FOR LOOP :
+"""
+syntax :
+for variable in sequence:
+    #code
+"""
+
+for i in range(6):
+    print(i)
+
+for n in range(1,10,2):
+    print(n)
+
+for str in range(3):
+    print("Aayushi")
+
+
+#WHILE LOOP:
+"""
+syntax :
+variable initialization
+while condition:
+    # code
+    increment/decrement condition
+"""
+
+i = 0
+while(i<3):
+    print("Bhardwaj")
+    i = i + 1
